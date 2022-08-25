a3024fdbc4b58633d181ca94d638960ddcfb707c
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-08-25 19:07:45 +0900

    feat: テストコードのサンプル

---
+++ functions/src/student-accounts/student.spec.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from '@firebase/app';
+import { getFirestore, getDocs, collection } from '@firebase/firestore';
+
+describe('xrpl', () => {
+  it('private key', async () => {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyD7h7z5SNhVz_L9uJS5i4WLe7VACgaNoR8',
+      authDomain: 'edison-test-f6ac3.firebaseapp.com',
+      projectId: 'edison-test-f6ac3',
+      storageBucket: 'edison-test-f6ac3.appspot.com',
+      messagingSenderId: '739107341891',
+      appId: '1:739107341891:web:54fb0767d50f823c06b6f6',
+      measurementId: 'G-KEL769L1D2',
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const students = await getDocs(collection(db, 'student_accounts')).then((snapshots) => snapshots.docs.map((doc) => doc.data()));
+    console.log(students);
+
+    expect(false).toBeFalsy();
+  });
+});
