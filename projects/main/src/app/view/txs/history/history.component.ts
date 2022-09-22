7dc0990506a99d59919d21ca8e0636e9e2d97c73
KokiKumagai <94220982+KokiKumagai@users.noreply.github.com>
2022-09-22 16:46:59 +0900

    feat: date, denom, amountの表示

--- projects/main/src/app/view/txs/history/history.component.ts
+++ projects/main/src/app/view/txs/history/history.component.ts
@@ -1,5 +1,6 @@
+import { Order, History } from '../../../page/txs/history/history.component';
 import { Component, Input, OnInit } from '@angular/core';
-import { RenewableBidHistory } from '@local/common';
+import { StudentAccount } from '@local/common';
 
 @Component({
   selector: 'view-history',
@@ -8,17 +9,34 @@ import { RenewableBidHistory } from '@local/common';
 })
 export class HistoryComponent implements OnInit {
   @Input()
-  renewableBid?: RenewableBidHistory | null;
-  @Input()
-  createdAt?: Date | null;
-  @Input()
-  bidCreatedAt?: Date | null;
+  studentAccount?: StudentAccount | null;
   @Input()
   uupxAmount?: number | null;
   @Input()
   uspxAmount?: number | null;
+  @Input()
+  orders?: Order[] | null;
+  @Input()
+  histories?: History[] | null;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  powerType(type: boolean) {
+    if (!type) {
+      return 'utility';
+    } else {
+      return 'solar';
+    }
+  }
+  txType(type: boolean) {
+    if (!type) {
+      return 'ask';
+    } else {
+      return 'bid';
+    }
+  }
+
+
 }
--- projects/main/src/app/view/txs/history/history.module.ts
+++ projects/main/src/app/view/txs/history/history.module.ts
@@ -4,13 +4,24 @@ import { HistoryComponent } from './history.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { RouterModule } from '@angular/router';
 import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [HistoryComponent],
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, MaterialModule, PipesModule, ChartsModule, MatExpansionModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    MatChipsModule,
+    PipesModule,
+    ChartsModule,
+    MatExpansionModule,
+  ],
   exports: [HistoryComponent],
 })
 export class HistoryModule {}
