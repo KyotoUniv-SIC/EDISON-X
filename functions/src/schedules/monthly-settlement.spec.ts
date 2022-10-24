984c47bb378aebea188206c2835807c6ca11dadc
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-10-24 19:51:27 +0900

    feat: monthly-settlement test

--- functions/src/schedules/monthly-settlement.spec.ts
+++ functions/src/schedules/monthly-settlement.spec.ts
@@ -1,9 +1,19 @@
-import { Balance, BalanceSnapshot, DiscountPrice, MarketStatus, PrimaryAsk, StudentAccount } from '@local/common';
-import { Timestamp } from 'firebase/firestore';
+import {
+  Balance,
+  DailyPayment,
+  InsufficientBalance,
+  MonthlySettlement,
+  NormalAskHistory,
+  NormalBidHistory,
+  PrimaryAsk,
+  RenewableAskHistory,
+  RenewableRanking,
+  RenewableRewardSetting,
+  StudentAccount,
+} from '@local/common';
 
 describe('Monthly Settlement Test', () => {
   it('Create monthly-settlement', () => {
-    // WIP
     const students = [
       new StudentAccount({ id: 'test01', name: 'test01', room_id: 'higashi301' }),
       new StudentAccount({ id: 'test02', name: 'test02', room_id: 'koushi201' }),
@@ -36,95 +46,169 @@ describe('Monthly Settlement Test', () => {
         amount_uspx: '2000000',
       }),
     ];
+    const insufficientBalances = [
+      new InsufficientBalance({
+        id: 'insufficient01',
+        student_account_id: 'test01',
+        amount_utoken: '1000000',
+      }),
+      new InsufficientBalance({
+        id: 'insufficient02',
+        student_account_id: 'test02',
+        amount_utoken: '3000000',
+      }),
+      new InsufficientBalance({
+        id: 'insufficient03',
+        student_account_id: 'test02',
+        amount_utoken: '2000000',
+      }),
+    ];
     let purchase = 0;
     let sale = 0;
     for (const student of students) {
       const studentID = student.id;
-      const lastMonthBalance = await balance.listLatest(studentID);
-      const insufficiencies = (await insufficient_balance.listLastMonth(studentID)).reduce(
-        (sum, element) => sum + parseInt(element.amount_utoken),
-        0,
-      );
+      const lastMonthBalance = balances.filter((element) => element.student_account_id == studentID);
+      const insufficiencies = insufficientBalances
+        .filter((element) => element.student_account_id == studentID)
+        .reduce((sum, element) => sum + parseInt(element.amount_utoken), 0);
       const totalBalance = parseInt(lastMonthBalance[0].amount_uspx) + parseInt(lastMonthBalance[0].amount_uupx) - insufficiencies;
+      console.log(totalBalance);
       if (totalBalance >= 0) {
         purchase += totalBalance;
       } else {
         sale += -totalBalance;
       }
     }
-    const adminAccount = await admin_account.getByName('admin');
-    const primaryAsks = await primary_ask.listLastMonth();
-    const normalAsks = await normal_ask_history.listLastMonth(adminAccount[0].id);
-    const normalBids = await normal_bid_history.listLastMonth(adminAccount[0].id);
-    const renewableAsks = await renewable_ask_history.listLastMonth(adminAccount[0].id);
+    expect(purchase).toBe(76000000);
+    expect(sale).toBe(5000000);
+    // const adminAccount = [new AdminAccount({ id: 'admin01', name: 'admin' })];
+    const primaryAsks = [
+      new PrimaryAsk({ id: 'primary01', account_id: 'test01', price_ujpy: '27000000', amount_uupx: '120000000' }),
+      new PrimaryAsk({ id: 'primary02', account_id: 'test02', price_ujpy: '27000000', amount_uupx: '150000000' }),
+      new PrimaryAsk({ id: 'primary03', account_id: 'test03', price_ujpy: '27000000', amount_uupx: '100000000' }),
+      new PrimaryAsk({ id: 'primary04', account_id: 'test04', price_ujpy: '27000000', amount_uupx: '80000000' }),
+    ];
+    const normalAsks = [
+      new NormalAskHistory({
+        id: 'nask01',
+        type: 1,
+        account_id: 'admin01',
+        price_ujpy: '26000000',
+        amount_uupx: '2000000',
+        is_accepted: true,
+        contract_price_ujpy: '27200000',
+      }),
+    ];
+    const normalBids = [
+      new NormalBidHistory({
+        id: 'nbid01',
+        account_id: 'admin01',
+        price_ujpy: '27000000',
+        amount_uupx: '2000000',
+        is_accepted: true,
+        contract_price_ujpy: '27000000',
+      }),
+      new NormalBidHistory({
+        id: 'nbid02',
+        account_id: 'admin01',
+        price_ujpy: '27000000',
+        amount_uupx: '2000000',
+        is_accepted: false,
+        contract_price_ujpy: '27000000',
+      }),
+    ];
+    const renewableAsks = [
+      new RenewableAskHistory({
+        id: 'rask01',
+        type: 1,
+        account_id: 'admin01',
+        price_ujpy: '27000000',
+        amount_uspx: '3000000',
+        is_accepted: true,
+        contract_price_ujpy: '27500000',
+      }),
+    ];
     let income = 0;
     for (const ask of primaryAsks) {
       income += (parseInt(ask.price_ujpy) * parseInt(ask.amount_uupx)) / 1000000;
     }
+    console.log(income);
     for (const ask of normalAsks) {
       if (ask.is_accepted) {
-        income += (parseInt(ask.price_ujpy) * parseInt(ask.amount_uupx)) / 1000000;
+        income += (parseInt(ask.contract_price_ujpy) * parseInt(ask.amount_uupx)) / 1000000;
       }
     }
+    console.log(income);
+
     for (const bid of normalBids) {
       if (bid.is_accepted) {
-        income -= (parseInt(bid.price_ujpy) * parseInt(bid.amount_uupx)) / 1000000;
+        income -= (parseInt(bid.contract_price_ujpy) * parseInt(bid.amount_uupx)) / 1000000;
       }
     }
+    console.log(income);
+
     for (const ask of renewableAsks) {
       if (ask.is_accepted) {
-        income += (parseInt(ask.price_ujpy) * parseInt(ask.amount_uspx)) / 1000000;
+        income += (parseInt(ask.contract_price_ujpy) * parseInt(ask.amount_uspx)) / 1000000;
       }
     }
+    console.log(income);
+
     const date = new Date();
-    const rewardSetting = await renewable_reward_setting.getLatest();
+    const rewardSetting = new RenewableRewardSetting({
+      first_price_ujpy: '600000000',
+      second_price_ujpy: '300000000',
+      third_price_ujpy: '100000000',
+    });
     const reward =
       parseInt(rewardSetting.first_price_ujpy) + parseInt(rewardSetting.second_price_ujpy) + parseInt(rewardSetting.third_price_ujpy);
 
-    await monthly_settlement.create(
-      new MonthlySettlement({
-        year: date.getFullYear().toString(),
-        month: date.getMonth().toString(),
-        reward_ujpy: reward.toString(),
-        system_income_ujpy: income.toString(),
-        purchase_utoken: purchase.toString(),
-        sale_utoken: sale.toString(),
+    const monthlySettlement = new MonthlySettlement({
+      year: date.getFullYear().toString(),
+      month: date.getMonth().toString(),
+      reward_ujpy: reward.toString(),
+      system_income_ujpy: income.toString(),
+      purchase_utoken: purchase.toString(),
+      sale_utoken: sale.toString(),
+    });
+    console.log(monthlySettlement);
+    expect(monthlySettlement.reward_ujpy).toBe('1000000000');
+    expect(monthlySettlement.system_income_ujpy).toBe(((12150 + 54.4 - 54 + 82.5) * 1000000).toString());
+
+    const payments = [
+      new DailyPayment({
+        student_account_id: 'test01',
+        amount_insufficiency: '0',
+        amount_mwh: '3000000',
+        amount_uupx: '3000000',
+        amount_uspx: '0',
       }),
-    );
-
-    // const costSetting = await cost_setting.getLatest();
-    // // システム運用コスト
-    // const cost = !costSetting ? 0 : parseInt(costSetting.system_cost_ujpy);
-    // // 電気料金
-    // const electricity = !costSetting ? 150000 * 1000000 : parseInt(costSetting.electricity_cost_ujpy);
-
-    // const rewardSetting = await renewable_reward_setting.getLatest();
-    // const reward =
-    //   parseInt(rewardSetting.first_price_ujpy) + parseInt(rewardSetting.second_price_ujpy) + parseInt(rewardSetting.third_price_ujpy);
-
-    // // 0で割るのを避ける
-    // let price: number;
-    // if (purchase + sale) {
-    //   price =
-    //     (cost + electricity + reward - income + ((purchase - sale) * parseInt(primaryAsks[0].price_ujpy)) / 1000000) /
-    //     (((purchase + sale) * parseInt(primaryAsks[0].price_ujpy)) / 1000000);
-    // } else {
-    //   price = 0;
-    // }
-    // console.log('Discount price', price);
-
-    // await discount_price.create(
-    //   new DiscountPrice({
-    //     price_ujpy: Math.floor(price).toString(),
-    //     amount_purchase_utoken: purchase.toString(),
-    //     amount_sale_utoken: sale.toString(),
-    //   }),
-    // );
-
+      new DailyPayment({
+        student_account_id: 'test01',
+        amount_insufficiency: '0',
+        amount_mwh: '4000000',
+        amount_uupx: '2000000',
+        amount_uspx: '2000000',
+      }),
+      new DailyPayment({
+        student_account_id: 'test02',
+        amount_insufficiency: '2000000',
+        amount_mwh: '4000000',
+        amount_uupx: '2000000',
+        amount_uspx: '0',
+      }),
+      new DailyPayment({
+        student_account_id: 'test03',
+        amount_insufficiency: '0',
+        amount_mwh: '3000000',
+        amount_uupx: '0',
+        amount_uspx: '3000000',
+      }),
+    ];
     const uspxPercentages = [];
     for (const student of students) {
       const studentID = student.id;
-      const dailyPayments = await daily_payment.listLastMonth(studentID);
+      const dailyPayments = payments.filter((element) => element.student_account_id == studentID);
       let mwhCount = 0;
       let uspxCount = 0;
       for (const payment of dailyPayments) {
@@ -141,34 +225,26 @@ describe('Monthly Settlement Test', () => {
       uspxPercentages.push({ studentID, uspxPercentage });
     }
     const uspxSortedPercentages = uspxPercentages.sort((first, second) => second.uspxPercentage - first.uspxPercentage);
-    await renewable_ranking.create(
-      new RenewableRanking({
-        first_student_id: uspxSortedPercentages[0].studentID,
-        second_student_id: uspxSortedPercentages[1].studentID,
-        third_student_id: uspxSortedPercentages[2].studentID,
-      }),
-    );
+    const ranking = new RenewableRanking({
+      first_student_id: uspxSortedPercentages[0].studentID,
+      second_student_id: uspxSortedPercentages[1].studentID,
+      third_student_id: uspxSortedPercentages[2].studentID,
+    });
+    console.log(ranking);
+    expect(ranking.first_student_id).toBe('test03');
 
     // BalanceSnapshotが計算のトリガーなので分割している
-    const xrplTxs = new XrplMonthlyTx({ txs: [] });
-    for (const student of students) {
-      const studentID = student.id;
-      console.log(studentID, 'payment start');
-      const lastMonthBalance = await balance.listLatest(studentID);
-      await balance_snapshot.create(new BalanceSnapshot(lastMonthBalance[0]));
-      const primaryAsk = await balanceSnapshotOnCreate({ data: () => lastMonthBalance[0] }, null);
-      xrplTxs.txs.push(primaryAsk);
-    }
-    await xrpl_monthly_tx.create(xrplTxs);
-    // await Promise.all(
-    //   students.map(async (student) => {
-    //     const studentID = student.id;
-    //     const lastMonthBalance = await balance.listLatest(studentID);
-    //     await balance_snapshot.create(new BalanceSnapshot(lastMonthBalance[0]));
-    //     await balanceSnapshotOnCreate({ data: () => lastMonthBalance[0] }, null);
-    //   }),
-    // );
+    // const xrplTxs = new XrplMonthlyTx({ txs: [] });
+    // for (const student of students) {
+    //   const studentID = student.id;
+    //   console.log(studentID, 'payment start');
+    //   const lastMonthBalance = await balance.listLatest(studentID);
+    //   await balance_snapshot.create(new BalanceSnapshot(lastMonthBalance[0]));
+    //   const primaryAsk = await balanceSnapshotOnCreate({ data: () => lastMonthBalance[0] }, null);
+    //   xrplTxs.txs.push(primaryAsk);
+    // }
+    // // await xrpl_monthly_tx.create(xrplTxs);
+
     console.log('tx end');
   });
-  expect(true).toBeFalsy()
 });
