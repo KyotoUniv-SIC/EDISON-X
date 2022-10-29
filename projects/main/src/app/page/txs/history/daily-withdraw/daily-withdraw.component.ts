3a853ebb9edabd655aaa1cce086e0e43e4d793c9
KokiKumagai <94220982+KokiKumagai@users.noreply.github.com>
2022-10-29 13:13:41 +0900

    Revert "Revert "feat: detailページ""
    This reverts commit 7020afda2ca333c16f380eff838c7d57241f1148.

--- projects/main/src/app/page/txs/history/daily-withdraw/daily-withdraw.component.ts
+++ projects/main/src/app/page/txs/history/daily-withdraw/daily-withdraw.component.ts
@@ -1,10 +1,8 @@
+import { PaymentHistory } from '../history.component';
 import { Component, OnInit } from '@angular/core';
 import { Auth, authState } from '@angular/fire/auth';
 import { Timestamp } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
-import { NormalBidHistory, PrimaryBid } from '@local/common';
-import { NormalBidHistoryApplicationService } from 'projects/shared/src/lib/services/normal-bid-histories/normal-bid-history.application.service';
-import { PrimaryBidApplicationService } from 'projects/shared/src/lib/services/primary-bids/primary-bid.application.service';
 import { StudentAccountApplicationService } from 'projects/shared/src/lib/services/student-accounts/student-account.application.service';
 import { combineLatest, Observable } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
@@ -15,31 +13,12 @@ import { map, mergeMap } from 'rxjs/operators';
   styleUrls: ['./daily-withdraw.component.css'],
 })
 export class DailyWithdrawComponent implements OnInit {
-  normalBid$: Observable<NormalBidHistory | undefined> | undefined;
-  primaryBid$: Observable<PrimaryBid | undefined> | undefined;
-  createdAt$: Observable<Date | null> | undefined;
-  bidCreatedAt$: Observable<Date | null> | undefined;
-  createdAtPrimary$: Observable<Date | null> | undefined;
+  dailyWithdraw$: Observable<PaymentHistory | undefined> | undefined;
 
-  constructor(
-    private auth: Auth,
-    private route: ActivatedRoute,
-    private readonly studentAccApp: StudentAccountApplicationService,
-    private readonly normalBidApp: NormalBidHistoryApplicationService,
-    private readonly primaryBidApp: PrimaryBidApplicationService,
-  ) {
+  constructor(private auth: Auth, private route: ActivatedRoute, private readonly studentAccApp: StudentAccountApplicationService) {
     const user$ = authState(this.auth);
     const studentAccount$ = user$.pipe(mergeMap((user) => this.studentAccApp.getByUid$(user?.uid!)));
     const historyID$ = this.route.params.pipe(map((params) => params.history_id));
-    this.normalBid$ = combineLatest([studentAccount$, historyID$]).pipe(
-      mergeMap(([studentAccount, historyID]) => this.normalBidApp.get$(studentAccount.id, historyID)),
-    );
-    this.createdAt$ = this.normalBid$.pipe(map((bid) => (!bid ? null : (bid?.created_at as Timestamp).toDate())));
-    this.bidCreatedAt$ = this.normalBid$.pipe(map((bid) => (!bid ? null : (bid?.bid_created_at as Timestamp).toDate())));
-    this.primaryBid$ = combineLatest([studentAccount$, historyID$]).pipe(
-      mergeMap(([studentAccount, historyID]) => this.primaryBidApp.get$(studentAccount.id, historyID)),
-    );
-    this.createdAtPrimary$ = this.primaryBid$.pipe(map((bid) => (!bid ? null : (bid?.created_at as Timestamp).toDate())));
   }
 
   ngOnInit(): void {}
